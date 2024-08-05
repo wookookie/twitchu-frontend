d7a78425e1c61b7c2d077aba126168128b97779e
wookookie <wookookie404@gmail.com>
2024-08-06 00:24:23 +0900

    Remove SideMenu

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import { Box, Flex } from "@chakra-ui/react";
 import { ChatLayout } from "./components/chat/ChatLayout";
 import NavBar from "./components/NavBar";
-import SideMenu from "./components/SideMenu";
 
 function App() {
   return (
     <Flex height="100vh">
-      <SideMenu width="240px" />
       <Box width="100%">
         <NavBar />
         <div style={{ display: "flex", justifyContent: "center" }}>
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,10 +1,12 @@
-import { HStack } from "@chakra-ui/layout";
+import { HStack, Image } from "@chakra-ui/react";
+import logo from "../assets/logo.png";
 import AuthButtons from "./AuthButtons";
 import ColorModeSwitch from "./ColorModeSwitch";
 
 function NavBar() {
   return (
     <HStack padding="12px" justify="space-between">
+      <Image boxSize="40px" src={logo} alt="Logo" />
       <ColorModeSwitch />
       <AuthButtons />
     </HStack>
